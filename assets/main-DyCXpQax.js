e99a93b32448b5241f0107b613643f4fffdd412f
stilistica <119519958+stilistica@users.noreply.github.com>
2025-08-25 17:07:38 +0000

    Deploying to gh-pages from @ stilistica/mealco@af47abe8d32f0704774f3d3e611d0c1473a750c8 🚀

--- assets/main-DyCXpQax.js
+++ assets/main-DyCXpQax.js
@@ -23,4 +23,4 @@ import{S as g,P as p,N as C,K as h}from"./vendor-D41edCa4.js";(function(){const 
     </div>
   </li>
   `);R.innerHTML=X.join("");
-//# sourceMappingURL=main-DfgWdoZO.js.map
+//# sourceMappingURL=main-DyCXpQax.js.map
